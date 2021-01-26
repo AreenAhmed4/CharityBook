a9db32bf8e8a8804658cc40c11f96ca14458d2de
Areen Ahmed <58148507+AreenAhmed4@users.noreply.github.com>
2021-01-26 13:32:05 +0200

    MainPAgeTyped

---
+++ JS/MainPage.js
@@ -0,0 +1,8 @@
+var typed = new Typed('.typed', {
+    strings: ["Charity Book is the trusted place for your donation","Charity Book is the trusted place for your donation", "Charity Book is the trusted place for your donation"],
+    typeSpeed: 50, loop:true });
+  
+  
+  
+  AOS.init();
+  
